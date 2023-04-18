5d946932972b1ea09f742bca4560303581a17422
MachadoAndre92 <2181620@my.ipleiria.pt>
2023-04-18 23:45:50 +0100

    Register class and auth

--- src/routes/authenticated.jsx
+++ src/routes/authenticated.jsx
@@ -1,12 +1,18 @@
 import {createBrowserRouter} from "react-router-dom";
 import Dashboard from '../page/dashboard_page'
 import ErrorPage from "../error-page"
+import Register from '../page/register_page'
 
 
 export default createBrowserRouter([
     {
       path: "/",
       element: <Dashboard />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
       errorElement: <ErrorPage />,
     },
     
